3374a800ae082e8fe878cf7344b96a55e8dbd32a
Mike Manturov <insolor@users.noreply.github.com>
2012-10-01 10:08:16 +0400

    get_raws.bat now creates required directories

--- get_raws.bat
+++ get_raws.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 1251
 set path=c:\Program Files (x86)\GnuWin32\bin;%path%
-
+mkdir raw\objects\text
 del raws
 wget http://www.dfwk.ru/Служебная:Export/User:Insolor/translation/raw -O raws
 for /f "tokens=2 delims=[|" %%I in (raws) do (
@@ -9,5 +9,5 @@ echo %%I
 wget http://www.dfwk.ru/User:Insolor/translation/raw%%I -q -O - | ^
 iconv -c -f utf-8 -t cp1251 | ^
 eui processwiki > "raw\objects%%I.txt"
-diff "raw\objects.bak%%I.txt" "raw\objects%%I.txt" --binary > "diffs\%%~nI.diff"
+REM diff "raw\objects.bak%%I.txt" "raw\objects%%I.txt" --binary > "diffs\%%~nI.diff"
 )
