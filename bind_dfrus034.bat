bb880bb1e370f4a7cd2044f98750877088cefe2d
Insolor <Insolor@Insolor-PC>
2012-10-02 09:11:28 +0400

    Added service batch files

---
+++ bind_dfrus034.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Псевдокомпиляция ("связывание") патча в exe
+eubind -con dfrus034
---
+++ dfrus034_debug.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Запуск патча в режиме отладки
+chcp 1251
+eui dfrus034 debug d:\Games\df_34_11_ironhand_0_73\
+rem eui dfrus034 debug d:\Games\df_34_11_win\
