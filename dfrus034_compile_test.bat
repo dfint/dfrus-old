aab73084bdb8b93ed6f973cb07d8321db226d326
Insolor <Insolor@Insolor-PC>
2012-10-22 00:20:45 +0400

    dfrus034_compile_test.bat changed DF path

--- dfrus034_compile_test.bat
+++ dfrus034_compile_test.bat
@@ -1,5 +1,6 @@
 @echo off
 REM Трансляция патча в Си и последюущая компиляция при помощи MinGW
 REM Откомпилированная версия не работает, обещали пофиксить в Euphoria 4.0.5, ждем.
+set EUDIR=c:\eu4
 euc -gcc -con dfrus034.exw
-dfrus034.exe debug d:\Games\df_34_09_win\
+dfrus034.exe debug d:\Games\df_34_11_ironhand_0_73\
