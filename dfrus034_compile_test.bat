b4edf36ba4f65742a2bf256659e619858a3866ff
insolor <insolor@localhost>
2013-05-12 16:00:01 +0400

    dfrus034_compile_test.bat: added chcp 1251

--- dfrus034_compile_test.bat
+++ dfrus034_compile_test.bat
@@ -1,6 +1,8 @@
 @echo off
 REM Трансляция патча в Си и последюущая компиляция при помощи MinGW
 REM Откомпилированная версия не работает, обещали пофиксить в Euphoria 4.0.6, ждем.
+REM Добавил chcp, т.к. "его" в кодировке 808 звучит нецензурно :)
+chcp 1251
 set EUDIR=c:\eu4
 euc -gcc -con dfrus034.exw
 "c:\Program Files (x86)\tools\upx308w\upx.exe" dfrus034.exe
