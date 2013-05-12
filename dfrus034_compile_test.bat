1f7a645e0f07af3e60ba7641e2f8535a1ad12e14
insolor <insolor@localhost>
2013-05-12 15:18:30 +0400

    dfrus034_compile_test.bat: added compression with upx

--- dfrus034_compile_test.bat
+++ dfrus034_compile_test.bat
@@ -1,6 +1,7 @@
 @echo off
 REM Трансляция патча в Си и последюущая компиляция при помощи MinGW
-REM Откомпилированная версия не работает, обещали пофиксить в Euphoria 4.0.5, ждем.
+REM Откомпилированная версия не работает, обещали пофиксить в Euphoria 4.0.6, ждем.
 set EUDIR=c:\eu4
 euc -gcc -con dfrus034.exw
+"c:\Program Files (x86)\tools\upx308w\upx.exe" dfrus034.exe
 dfrus034.exe debug d:\Games\df_34_11_ironhand_0_73\
